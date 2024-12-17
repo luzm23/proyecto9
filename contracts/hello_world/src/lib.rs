440cd54e0ed238f880e6f5450ad1b5d3b6cf596a
root <root@Delluz.>
2024-12-17 13:29:37 -0600

    Contrato

--- contracts/hello_world/src/lib.rs
+++ contracts/hello_world/src/lib.rs
@@ -1,1 +1,26 @@
+#![no_std]
+
+use soroban_sdk::{contractimpl, Env};
+
+pub struct HelloWorldContract;
+
+#[contractimpl]
+impl HelloWorldContract {
+    pub fn say_hello(env: Env) -> &'static str {
+        "Hello, world!"
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use soroban_sdk::Env;
+
+    #[test]
+    fn test_say_hello() {
+        let env = Env::default();
+        let contract = HelloWorldContract;
+        assert_eq!(contract.say_hello(env), "Hello, world!");
+    }
+}
 
